42147bea22a45a9d45360fe33404bd20b0cccb60
VC Videos <228714185+video1998@users.noreply.github.com>
2025-08-26 17:13:43 +0530

    Create vc1.php

---
+++ vc1.php
@@ -0,0 +1,5 @@
+{"ReturnValue":"success","urls":[
+"https://github.com/user-attachments/assets/421fb968-74aa-4892-a4b2-e02ea84a8e5e",
+"https://github.com/user-attachments/assets/22cfe0ae-e4f2-4778-84e0-48c07980cb2b",
+"https://github.com/user-attachments/assets/682c6737-a234-4aa2-8d69-453b325431c3"
+]}
