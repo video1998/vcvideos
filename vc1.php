7dc18e04830e4e8773804fab253fb0709c278499
VC Videos <228714185+video1998@users.noreply.github.com>
2025-08-26 17:49:56 +0530

    Update vc1.php

--- vc1.php
+++ vc1.php
@@ -1,10 +1,8 @@
 {"ReturnValue":"success","urls":[
 
-"https://github.com/user-attachments/assets/c8b938ba-1e5f-457f-ba33-6951ecc73714",
+"https://github.com/user-attachments/assets/eaf00356-bf85-4d13-bbb7-7dfaf4604991",
 
-"https://github.com/user-attachments/assets/9e882c1c-bf78-4f18-bd39-37406af8fa3c",
+"https://github.com/user-attachments/assets/4b887231-92fb-481d-9e11-1d4f78aaa8bd",
 
-"https://github.com/user-attachments/assets/08f47759-7b75-40f7-a97d-afc100be97db",
-
-"https://github.com/user-attachments/assets/38db4efa-2858-4987-a976-71d26ca1ddc5"
+"https://github.com/user-attachments/assets/cfbccb98-a1b0-49f1-b6b7-ab6b9d7c69d5"
 ]}
