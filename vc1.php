5d9c27a2bd92d4cf51136839588f2a583173ad36
VC Videos <228714185+video1998@users.noreply.github.com>
2025-08-26 17:45:33 +0530

    Update vc1.php

--- vc1.php
+++ vc1.php
@@ -2,15 +2,9 @@
 
 "https://github.com/user-attachments/assets/c8b938ba-1e5f-457f-ba33-6951ecc73714",
 
-
-
 "https://github.com/user-attachments/assets/9e882c1c-bf78-4f18-bd39-37406af8fa3c",
 
-
-
 "https://github.com/user-attachments/assets/08f47759-7b75-40f7-a97d-afc100be97db",
-
-
 
 "https://github.com/user-attachments/assets/38db4efa-2858-4987-a976-71d26ca1ddc5"
 ]}
