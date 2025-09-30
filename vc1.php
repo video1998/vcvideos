561f6a2188a856b82d5467877d95f29b0d3d4130
video1998 <freefirevideo1998@gmail.com>
2025-09-30 17:18:40 +0530

    Update vc1.php

--- vc1.php
+++ vc1.php
@@ -4,6 +4,28 @@
 
 "https://github.com/user-attachments/assets/81362a9c-7738-4e69-b37d-f634b2f6cee8",
 
-"https://github.com/user-attachments/assets/d7f971fa-98b2-4778-8b27-94af00bb6ad3"
+"https://github.com/user-attachments/assets/d7f971fa-98b2-4778-8b27-94af00bb6ad3",
+
+"https://youtu.be/dSw-ljtd0UA",
+
+"https://youtu.be/s3FrVZ7ciRQ",
+
+"https://youtu.be/qUTe7pkFFtY",
+
+"https://youtu.be/QA5DuO9dW6Y",
+
+"https://youtube.com/shorts/i078gRnO7f0",
+
+"https://youtube.com/shorts/v_SLSY2WXYY",
+
+"https://youtube.com/shorts/4yDC3r81Aqc",
+
+"https://youtube.com/shorts/9uYAXP0LybE",
+
+"https://youtube.com/shorts/bMEwdtYNDsY",
+
+"https://youtube.com/shorts/eU4acaLZBw4",
+
+"https://youtube.com/shorts/4yDC3r81Aqc"
 
 ]}
